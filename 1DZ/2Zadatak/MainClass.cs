5185ca9a6a45ea8e20588f480765f949093292fd
fdzidic <filipdzidic@gmail.com>
2016-10-20 16:56:13 +0200

    Finished 2nd task.

---
+++ 1DZ/2Zadatak/MainClass.cs
@@ -0,0 +1,42 @@
+﻿using _2Zadatak;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DZ
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            IGenericList<string> list = new GenericList<string>();
+            list.Add("jedan");
+            Console.WriteLine(list);
+            list.Add("dva");
+            Console.WriteLine(list);
+            list.Add("tri");
+            Console.WriteLine(list);
+            list.Add("četiri");
+            Console.WriteLine(list);
+            list.Add("pet");
+            Console.WriteLine(list);
+
+            Console.WriteLine(list.Contains("nula"));
+
+            list.RemoveAt(0);
+            Console.WriteLine(list);
+            list.Remove("tri");
+            Console.WriteLine(list);
+            Console.WriteLine(list.Count);
+            Console.WriteLine(list.Remove("ayylmao"));
+            Console.WriteLine(list.RemoveAt(5));
+            list.Clear();
+            Console.WriteLine(list);
+            Console.WriteLine(list.Count);
+            Console.ReadKey();
+        }
+
+    }
+}
